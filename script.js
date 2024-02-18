ced3571a52b437379f18f28546a3750f79f862c3
fojletuhin <fojletuhin@gmail.com>
2024-02-18 23:46:01 +0600

    add discount price

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const allSeat = document.getElementsByClassName('seat');
 let totalSeatLeft = 40;
 let totalCount = 0;
 const arr = [];
-let cost=0;
+let cost = 0;
 
 for (const seat of allSeat) {
   seat.addEventListener('click', function (e) {
 
-    
+
 
     if (arr.includes(seat)) {
       alert('Do not add same seat twice')
     }
-    else{
+    else {
       arr.push(seat)
       totalSeatLeft -= 1;
       totalCount += 1;
@@ -35,31 +35,70 @@ for (const seat of allSeat) {
         const SinglePrice = document.createElement("p");
         SinglePrice.innerText = 550;
 
-       
-
         li.appendChild(p)
         li.appendChild(Economy)
         li.appendChild(SinglePrice)
         elements.appendChild(li)
 
-        const totalPriceValue=document.getElementById('total-price')
-
-        cost= cost+550;
-        totalPriceValue.innerText=cost;
-
-
-     
+        const totalPriceValue = document.getElementById('total-price')
+        cost = cost + 550;
+        totalPriceValue.innerText = cost;
+        coupon(totalCount)
+        apply(cost)
       }
       else {
         alert("Sorry, You can't buy 5 or more ticket");
       }
-
-
-     
     }
-
-
-    
   })
 }
 
+
+
+function coupon(totalCount) {
+
+  const CouponBtn = document.getElementById('Coupon-btn')
+  if (totalCount === 4) {
+    CouponBtn.classList.remove('hidden');
+  }
+  else {
+    CouponBtn.classList.add('hidden');
+  }
+}
+
+
+function apply() {
+
+  const totalPrice = document.getElementById('total-price').innerText;
+  const convertedTotalPrice=parseInt(totalPrice);
+
+  const discountValue = document.getElementById('discount');
+  const grandTotalValue = document.getElementById('grand-total');
+  const input = document.getElementById('input').value;
+
+
+
+  if (input === "NEW15") {
+    const grandTotal = convertedTotalPrice - (convertedTotalPrice * 15) / 100;
+    grandTotalValue.innerText = grandTotal;
+
+    const discountPrice= (convertedTotalPrice * 15) / 100;
+    discountValue.innerText=discountPrice;
+  }
+
+  else if (input === "Couple 20") {
+    const grandTotal = convertedTotalPrice - (convertedTotalPrice * 20) / 100;
+    grandTotalValue.innerText = grandTotal;
+
+    const discountPrice= (convertedTotalPrice * 20) / 100;
+    discountValue.innerText=discountPrice;
+  }
+     else{
+      grandTotalValue.innerText=convertedTotalPrice;
+     }
+
+
+}
+
+
+console.log(input)
